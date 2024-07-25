12c9442c10ec3ee795da30204ed4f936e03f389a
Iz <israel.landes16@gmail.com>
2024-07-25 23:21:40 +0000

    good

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -2,10 +2,24 @@
 
 import React from 'react'
 
-const UserPage = () => {
-  return (
-    <div>UserPage</div>
-  )
+interface User {
+  id: number
+  name: string;
 }
 
-export default UserPage
+const UsersPage = async () => {
+
+const res = await fetch('http://jsonplaceholder.typicode.com/users', { cache: 'no-store' });
+const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
+  )
+}
+export default UsersPage
